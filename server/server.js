8e718f1588a530a927944ee8988836f98ca47064
IgnorePT <n3ls6n@hotmail.com>
2018-03-06 14:28:15 +0000

    Add ID Routing to ur Todos

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -32,6 +33,38 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+//GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    //validate ID (ObjectID);
+        // 404 - Not Found Empty Body
+    if(!ObjectID.isValid(id)){
+        console.log('Todo id is not valid');
+        return  res.status(404).send();
+    }
+    
+    //findByID
+        //Success
+            //If todo - send it back
+            //If no todo - send back 404 - empty body
+
+        Todo.findById(id).then((todo) => {
+            if(!todo){
+                console.log('Todo not found');
+                return  res.status(404).send();
+            }
+            res.send(todo);
+        }), (err) => {
+            console.log('Unable to fetch todos');            
+            res.status(404).send();
+        }
+        //Error
+            //400 - Sendo Nothing
+
+
 });
 
 app.listen(3000, () => {
