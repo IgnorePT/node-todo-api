9c6013578040b292111100f8891c06b8b40789a5
IgnorePT <n3ls6n@hotmail.com>
2018-03-09 12:47:20 +0000

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var url = "mongodb://localhost:27017/TodoApp";
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url);
